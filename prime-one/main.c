924d7ccfe284a53e65893435d21ec7f71d080820
bitschrauber <mgropp@web.de>
2021-02-07 13:35:24 +0100

    Initial Commit

---
+++ prime-one/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  prime-one
+//
+//  Created by Matthias Gropp on 07.02.21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
